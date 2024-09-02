abe9bc161bb51031f8f8295f6e7b4552b5581068
mkunta1 <mahisas010@gmail.com>
2024-09-02 17:54:49 -0400

    Finalize utils module
    Finalize utils module

--- utils.mahitha.py
+++ utils.mahitha.py
@@ -1,59 +1,22 @@
-''' ITERATION 4
+''' Final Example
 
 Module: Mahitha Analytics - Reusable Module for My Data Analytics Projects
 
-
-This module provides a simple, reusable foundation for my analytics projects.
-
-Process:
-In this fourth iteration, I introduce some basic statistics using Python.
-    - min() is a built in function to find the smallest value passed in
-    - max() is a built in function to find the largest value passed in
-    - The statistics module offers methods to calculate mean and standard deviation.
-
 '''
-#####################################
-# Import modules at the top.
-#####################################
-
-# In Python, we can import modules to add extra tools and functions.
-# Below, we're importing:
-# - `statistics`:  This gives us tools to calculate things like averages.
-# Use CTRL F and type statistics to see where it is used in the code.
-# Did you find statistics.mean()?
-# Did you find statistics.stdev()?
 
 import statistics
 
 
-#####################################
-# Declare global variables
-#####################################
-
-# Boolean variables to indicate if the company has international clients and is privately owned.
-
 has_international_clients: bool = True
-is_provately_held: bool = True
-
-#Interger variable for the number of years in operation 
 years_in_operation: int = 10
-number_of_months:   int = 12
-
-
-#Float variable for the average client satisfaction score 
 average_client_satisfaction: float = 4.7 
-
-#List of strings representing the skills pffered by the company 
 skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
-courses_offered: list = ["math", "science", "english"]
-
-#List of floats representing individual client satisfaction scores 
 client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
 temperature_values: list = [40.8, 37.6, 44.9, 25.0, 44.7]
 
-
 #####################################
-# Calculate Statistics
+# Calculate Basic Statistics using built in functions.
+
 #####################################
 
 # Calculate basic stats using built-in functions min(), max(), and statistics module functions mean() and stdev().
@@ -66,31 +29,23 @@ max_temp:  float = max(temperature_values)
 mean_temp:  float = statistics.mean(temperature_values)
 stdev_temp:  float = statistics.stdev(temperature_values)
 
-#####################################
-# Declare a global variable named byline.
-# Make it a multiline f-string to show our information.
-#####################################
-
 byline: str = f"""
 -------------------------------------------------------------
 Mahitha Analytics : Delivering Professional Insights
 -------------------------------------------------------------
 Has International Clients:  {has_international_clients}
-Is Privately held:  {is_provately_held}
 Years in Operation:  {years_in_operation}
-Number of Months:  {number_of_months}
 Skills Offered:  {skills_offered}
-Courses Offered:  {courses_offered}
-Temperature values (F):  {temperature_values}
 Client Satisfaction Scores:  {client_satisfaction_scores}
 Minimum Client Satisfacton Score:  {min_score}
 Maximum Client Satisfaction Score:  {max_score}
 Average Client Satisfaction Score:  {mean_score}
-Standard Deviation of Client Satisfaction Scores:  {stdev_score}
+Standard Deviation of Client Satisfaction Scores:  {stdev_score:.2f}
 Minimum Temperature (F):  {min_temp}
 Maximum Temperature (F):  {max_temp}
 Mean Temperature (F):  {mean_temp}
 Standard Deviation of Temperature (F):  {stdev_temp}
+
 """
 
 #####################################
